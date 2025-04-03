a75768672039465a6e1bbd82f59599a62e8bb12e
CaioHVectorA <caihebatista@gmail.com>
2025-04-03 11:50:11 -0300

    Add some comments

--- feira-de-domingo.c
+++ feira-de-domingo.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 void ordena_frutas(char** frutas) {
-
 }
 
 void printa_frutas(char** frutas, int qtd) {
     printf("--------------- FRUTAS ------------\n");
     for (int i = 0; i < qtd; i++) {
-        printf("%s\n",frutas[i]);
+        printf("%s\n",frutas[i]); // printa cada fruta e quebra a linha
     }
     printf("-----------------------------------\n");
 } 
@@ -16,12 +15,14 @@ int main() {
     const int MAX_TAMANHO_STRING = 20; // o tamanho máximo da string de frutas, 20 caracteres
     printf("Digite a quantidade de frutas a serem lidas \n");
     scanf("%d", &qtd); // lê a quantidade p/ alocação dinâmica
-    char **frutas = (char**)malloc(qtd * sizeof(char*));
+    char **frutas = (char**)malloc(qtd * sizeof(char*)); // aloca de forma dinamica
+    // -- lê cada fruta separadamente e aloca
     printf("Digite as frutas: \n");
     for (int i = 0; i < qtd; i++) {
         frutas[i] = (char*)malloc(MAX_TAMANHO_STRING * sizeof(char));
         scanf("%s", frutas[i]);
     }
+    // --
     printa_frutas(frutas, qtd);
     return 0;
 }
